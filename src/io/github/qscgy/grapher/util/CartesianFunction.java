c2f31c4f3ee20181a3423681bd34860211f6ab3f
qscgy <sam.ehrenstein@gmail.com>
2015-04-13 13:33:04 -0400

    added class for Cartesian functions

---
+++ src/io/github/qscgy/grapher/util/CartesianFunction.java
@@ -0,0 +1,14 @@
+package io.github.qscgy.grapher.util;
+
+public class CartesianFunction implements Function{
+  
+  String expr;
+  
+  public CartestianFunction(String expr){
+    this.expr=expr;
+  }
+  
+  public double f(double x){
+    
+  }
+}
